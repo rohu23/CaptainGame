11c021a1925258beade04c9dd1ae0a832c14450a
rohu23 <rohittallapragada44@gmail.com>
2024-06-07 17:17:10 -0400

    Updates to code, and design change

--- app/src/main/java/com/rtee/captaingame/MainActivity.kt
+++ app/src/main/java/com/rtee/captaingame/MainActivity.kt
@@ -12,9 +12,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -28,7 +30,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CaptainGameTheme {
-                    captainGame()
+                    CaptainGame()
                 }
             }
         }
@@ -40,76 +42,76 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun GreetingPreview() {
     CaptainGameTheme {
-        captainGame()
+        CaptainGame()
     }
 }
 
 @Composable
-fun captainGame(){
-    val treasuresFound = remember { mutableIntStateOf(0)}
-    val direction = remember { mutableStateOf("North") }
-    val stormOrTreasure = remember { mutableStateOf("") }
-    val healthPoints = remember { mutableIntStateOf(100) }
+fun CaptainGame(){
+    var treasuresFound by remember { mutableIntStateOf(0)}
+    var direction by remember { mutableStateOf("North") }
+    var stormOrTreasure by remember { mutableStateOf("") }
+    var healthPoints by remember { mutableIntStateOf(100) }
 
-    if (healthPoints.intValue > 0) {Column(
+    if (healthPoints> 0) {Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
         Spacer(modifier = Modifier.padding(16.dp))
-        Text(text = "Treasures Found: ${treasuresFound.value}")
-        Text(text = "Direction Headed: ${direction.value}")
-        Text(text = "Health Points: ${healthPoints.value}")
+        Text(text = "Treasures Found: $treasuresFound")
+        Text(text = "Direction Headed: $direction")
+        Text(text = "Health Points: $healthPoints")
 
         Button(onClick = {
-            direction.value = "North"
+            direction = "North"
             if (Random.nextBoolean()) {
-                treasuresFound.value += 1
-                stormOrTreasure.value = "Found a Treasure!"
+                treasuresFound += 1
+                stormOrTreasure = "Found a Treasure!"
             } else {
-                stormOrTreasure.value = "Storm Ahead!"
-                healthPoints.value -= 10
+                stormOrTreasure = "Storm Ahead!"
+                healthPoints -= 10
             }
         }) {
             Text(text = "North")
         }
         Button(onClick = {
-            direction.value = "East"
+            direction = "East"
             if (Random.nextBoolean()) {
-                treasuresFound.value += 1
-                stormOrTreasure.value = "Found a Treasure!"
+                treasuresFound += 1
+                stormOrTreasure = "Found a Treasure!"
             } else {
-                stormOrTreasure.value = "Storm Ahead!"
-                healthPoints.value -= 10
+                stormOrTreasure = "Storm Ahead!"
+                healthPoints -= 10
             }
         }) {
             Text(text = "East")
         }
         Button(onClick = {
-            direction.value = "West"
+            direction = "West"
             if (Random.nextBoolean()) {
-                treasuresFound.value += 1
-                stormOrTreasure.value = "Found a Treasure!"
+                treasuresFound += 1
+                stormOrTreasure = "Found a Treasure!"
             } else {
-                stormOrTreasure.value = "Storm Ahead!"
-                healthPoints.value -= 10
+                stormOrTreasure = "Storm Ahead!"
+                healthPoints -= 10
             }
         }) {
             Text(text = "West")
         }
         Button(onClick = {
-            direction.value = "South"
+            direction = "South"
             if (Random.nextBoolean()) {
-                treasuresFound.value += 1
-                stormOrTreasure.value = "Found a Treasure!"
+                treasuresFound += 1
+                stormOrTreasure = "Found a Treasure!"
             } else {
-                stormOrTreasure.value = "Storm Ahead!"
-                healthPoints.value -= 10
+                stormOrTreasure = "Storm Ahead!"
+                healthPoints -= 10
             }
         }) {
             Text(text = "South")
         }
-        Text(text = stormOrTreasure.value)
+        Text(text = stormOrTreasure)
     }
     }else{
         Column(
@@ -120,10 +122,10 @@ fun captainGame(){
         {
             Spacer(modifier = Modifier.padding(16.dp))
             Text(text = "Game Over")
-            Button(onClick = { healthPoints.value = 100
-                treasuresFound.value = 0
-                direction.value = "North"
-                stormOrTreasure.value = "" } ) {
+            Button(onClick = { healthPoints = 100
+                treasuresFound = 0
+                direction = "North"
+                stormOrTreasure = "" } ) {
                 Text(text = "Restart")
             }
 
